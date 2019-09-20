a5780ff932f5392152036160c21c40376d6810a2
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-20 11:03:38 +0200

    add entry module and the major class

---
+++ src/abstractModule.ts
@@ -0,0 +1,6 @@
+/**
+ * @class AbstractModule
+ * @description The main interface of this package. The abstractModule class should be implemented by every rokkit.ts
+ * module to unify the functionalities and their apis.
+ */
+export abstract class AbstractModule {}
