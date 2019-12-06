c401668ce381edb209fd3969ab5cba2cae5ada28
Jann Deterling <jann.deterling@thoughtworks.com>
2019-12-06 11:32:00 +0100

    [Jann] adds pipeline configuration

--- src/abstractModule.ts
+++ src/abstractModule.ts
@@ -3,4 +3,10 @@
  * @description The main interface of this package. The abstractModule class should be implemented by every rokkit.ts
  * module to unify the functionalities and their apis.
  */
-export abstract class AbstractModule {}
+export abstract class AbstractModule {
+  public abstract async initializeModule(configuration: any): Promise<void>;
+  public abstract async injectDependencies(
+    instanceMap: Map<string, any>
+  ): Promise<void>;
+  public abstract async shoutDownModule(): Promise<void>;
+}
