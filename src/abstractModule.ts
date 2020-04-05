5fdf03422c1d30830e8ef19477752361e36efbd0
Jann Deterling <jann.deterling@thoughtworks.com>
2020-04-05 16:14:30 +0200

    [Jann] Remove istanceMap as it is not needed, use config as constructor param

--- src/abstractModule.ts
+++ src/abstractModule.ts
@@ -3,19 +3,12 @@
  * @description The main interface of this package. The abstractModule class should be implemented by every rokkit.ts
  * module to unify the functionalities and their apis.
  */
-export abstract class AbstractModule {
-  protected instanceMap: Map<string, any>;
-
-  constructor() {
-    this.instanceMap = new Map();
+export abstract class AbstractModule<T> {
+  protected readonly configuration: T;
+  constructor(configuration: T) {
+    this.configuration = configuration;
   }
 
-  public async injectDependencies(
-    instanceMap: Map<string, any>
-  ): Promise<void> {
-    this.instanceMap = instanceMap;
-  }
-
-  public abstract async runModule(configuration: any): Promise<void>;
+  public abstract async runModule(): Promise<void>;
   public abstract async shoutDownModule(): Promise<void>;
 }
